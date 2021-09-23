69e73d1f4afcf7f8e0ea686decabe8fbca1c6e4c
himanshulal9 <himanshulal9@gmail.com>
2021-09-23 13:29:14 +0530

    added flash message

--- app.js
+++ app.js
@@ -7,13 +7,36 @@ const IndexRouter = require("./routes/index.route");
 const AuthRouter = require("./routes/auth.route");
 const UserRouter = require("./routes/user.route");
 require("dotenv").config();
+const session = require("express-session");
+const connectFlash = require("connect-flash");
 
+//initialization
 const app = express();
 app.use(morgan("dev"));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+//init session
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      //secure : true,
+      httpOnly: true,
+    },
+  })
+);
+
+//all about flash messages
+app.use(connectFlash());
+app.use((req, res, next) => {
+  res.locals.messages = req.flash();
+  next();
+});
 
 //defining Routes
 app.use("/", IndexRouter);
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,17 +1,21 @@
-const { User } = require("../models/user.model");
-
 const router = require("express").Router();
+const flash = require("connect-flash");
+const { User } = require("../models/user.model");
 
 router.get("/login", async (req, res, next) => {
   res.render("login");
 });
 
-router.get("/register", async (req, res, next) => {
-  res.render("register");
-});
-
 router.post("/login", async (req, res, next) => {
   res.render("login");
+});
+
+router.get("/register", async (req, res, next) => {
+  req.flash("error", "some Extra Error");
+  req.flash("key", "Some value");
+
+  // res.render("register");
+  res.redirect("/auth/login");
 });
 
 router.post("/register", async (req, res, next) => {
